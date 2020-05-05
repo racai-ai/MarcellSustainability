3f4175cc0ed610175a8426d3ef4f7dae3f7fa1a7
Radu Ion <radu@racai.ro>
2020-05-05 18:08:29 +0300

    More exporting and formatting

--- RO_annotator/docker/cpan-install-homedir.sh
+++ RO_annotator/docker/cpan-install-homedir.sh
@@ -3,5 +3,6 @@
 export PERL5VER=`perl -V:version | grep -Po "5\.[0-9]+(?:\.[0-9]+)?"`
 export PERL5LIB=/root/perl/lib/x86_64-linux-gnu/perl/$PERL5VER:/root/perl/share/perl/$PERL5VER
 echo "PERL5LIB contains:"
+echo -n "  "
 perl -e 'print(join("\n  ", @INC) . "\n")'
 cpan -i File::HomeDir
--- RO_annotator/docker/perl-check-install.sh
+++ RO_annotator/docker/perl-check-install.sh
@@ -2,12 +2,11 @@
 
 # If perl version changes... No problem, we have PERL5VER now.
 export PERL5VER=`perl -V:version | grep -Po "5\.[0-9]+(?:\.[0-9]+)?"`
-echo "perl-check-install: we got perl version = $PERL5VER"
-
 # Set additional search directories for @INC
 export PERL5LIB=/root/perl/lib/x86_64-linux-gnu/perl/$PERL5VER:/root/perl/share/perl/$PERL5VER
-echo "perl-check-install: added perl libraries = $PERL5LIB"
-
+echo "PERL5LIB contains:"
+echo -n "  "
+perl -e 'print(join("\n  ", @INC) . "\n")'
 # Check perl installation
 cd /root/nlpipe
 perl -c PipTTL.pl
