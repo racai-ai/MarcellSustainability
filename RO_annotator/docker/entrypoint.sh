ab472c286e1d4c47d6944ab5e69681dde9117fab
Radu Ion <radu@racai.ro>
2020-05-05 19:05:00 +0300

    Debug pytest

--- RO_annotator/docker/entrypoint.sh
+++ RO_annotator/docker/entrypoint.sh
@@ -8,18 +8,4 @@ cron &
 
 cd /ner/IATE-EUROVOC-Annotator/ ; ./annotate server 9001 &
 
-# NLPipe tests and server start commands
-export PERL5VER=`perl -V:version | grep -Po "5\.[0-9]+(?:\.[0-9]+)?"`
-export PERL5LIB=/root/perl/lib/x86_64-linux-gnu/perl/$PERL5VER:/root/perl/share/perl/$PERL5VER
-
-cd /root/nlpipe
-pytest -s -v tests
-
-if [ $? -ne 0 ]; then
-	exit 1
-else
-	./start.ws
-fi
-# End NLPipe commands
-
 /usr/sbin/apachectl -D FOREGROUND
