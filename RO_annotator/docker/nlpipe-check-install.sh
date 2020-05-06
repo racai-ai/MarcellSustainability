036e38062dcbe03a8d2e94a5b134625f6c8a72e1
Radu Ion <radu@racai.ro>
2020-05-06 08:43:05 +0300

    Sleep more

--- RO_annotator/docker/nlpipe-check-install.sh
+++ RO_annotator/docker/nlpipe-check-install.sh
@@ -3,7 +3,7 @@
 /ner/fastText-0.1.0-mod/fasttext server /ner/corola.300.20.5.bin 8001 &
 /ner/ner_server.sh &
 
-sleep 30s
+sleep 60s
 
 export PERL5VER=`perl -V:version | grep -Po "5\.[0-9]+(?:\.[0-9]+)?"`
 export PERL5LIB=/root/perl/lib/x86_64-linux-gnu/perl/$PERL5VER:/root/perl/share/perl/$PERL5VER
