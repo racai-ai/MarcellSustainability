e0e8df9036e86806b5a0901a4655d67541e35674
Radu Ion <radu@racai.ro>
2020-05-06 10:47:06 +0300

    Python tests after the fact

--- RO_annotator/docker/entrypoint.sh
+++ RO_annotator/docker/entrypoint.sh
@@ -11,7 +11,7 @@ cd /ner/IATE-EUROVOC-Annotator/ ; ./annotate server 9001 &
 export PERL5VER=`perl -V:version | grep -Po "5\.[0-9]+(?:\.[0-9]+)?"`
 export PERL5LIB=/root/perl/lib/x86_64-linux-gnu/perl/$PERL5VER:/root/perl/share/perl/$PERL5VER
 
-cd /root/nlpipe
-./start.ws
+cd /root/nlpipe/
+./start-ws.sh
 
 /usr/sbin/apachectl -D FOREGROUND
--- RO_annotator/docker/nlpipe-check-install.sh
+++ RO_annotator/docker/nlpipe-check-install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-/ner/fastText-0.1.0-mod/fasttext server /ner/corola.300.20.5.bin 8001 &
-/ner/ner_server.sh &
-
-sleep 20s
-
-/usr/sbin/apachectl start
-
 export PERL5VER=`perl -V:version | grep -Po "5\.[0-9]+(?:\.[0-9]+)?"`
 export PERL5LIB=/root/perl/lib/x86_64-linux-gnu/perl/$PERL5VER:/root/perl/share/perl/$PERL5VER
 
